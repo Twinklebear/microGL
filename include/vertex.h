d2f1a32655a8df00c4e14bfbf93a423c3fedffff
Will <willusher.life@gmail.com>
2014-07-27 00:11:55 -0700

    Use floating point colors until we write to color buffer

--- include/vertex.h
+++ include/vertex.h
@@ -4,24 +4,11 @@
 #include <stdint.h>
 #include "vector4f.h"
 
-//Color masks for working with vertex colors
-#define RMASK 0xff000000
-#define GMASK 0x00ff0000
-#define BMASK 0x0000ff00
-
-#define SET_RED(P, C)	(P = ((P & ~RMASK) | (C << 24)))
-#define SET_GREEN(P, C)	(P = ((P & ~GMASK) | (C << 16)))
-#define SET_BLUE(P, C)	(P = ((P & ~BMASK) | (C << 8)))
-#define GET_RED(P)   ((P & RMASK) >> 24)
-#define GET_GREEN(P) ((P & GMASK) >> 16)
-#define GET_BLUE(P)  ((P & BMASK) >> 8)
-
 /*
  * Describes a vertex by its color and position
  */
 typedef struct vertex_t {
-	vector4f_t pos;
-	uint32_t color;
+	vector4f_t pos, color;
 } vertex_t;
 
 /*
--- src/vertex.c
+++ src/vertex.c
@@ -6,13 +6,7 @@
 vertex_t vertex_new(vector4f_t pos, vector4f_t color){
 	vertex_t vertex;
 	vertex.pos = pos;
-	uint8_t r = (uint8_t)(fmax(fmin(color.x, 1.f), 0.f) * 255);
-	uint8_t b = (uint8_t)(fmax(fmin(color.y, 1.f), 0.f) * 255);
-	uint8_t g = (uint8_t)(fmax(fmin(color.z, 1.f), 0.f) * 255);
-	SET_RED(vertex.color, r);
-	SET_GREEN(vertex.color, g);
-	SET_BLUE(vertex.color, b);
-
+	vertex.color = color;
 	return vertex;
 }
 
